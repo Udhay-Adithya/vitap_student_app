8bfde8450feae47bb9f6f519d741cf021bf44997
Udhay-Adithya <udhayxd@gmail.com>
2026-06-19 16:14:16 +0530

    fix: ListTile background color or ink splashes may be invisible

--- lib/features/account/view/widgets/settings_tile.dart
+++ lib/features/account/view/widgets/settings_tile.dart
@@ -41,50 +41,53 @@ class SettingTile extends StatelessWidget {
             endIndent: 28,
             thickness: 0.8,
           ),
-        ListTile(
-          tileColor:
-              tileColor ?? Theme.of(context).colorScheme.surfaceContainerLow,
-          titleTextStyle: Theme.of(context)
-              .textTheme
-              .bodyLarge
-              ?.copyWith(color: titleColor),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8),
-          ),
-          leading: Container(
-            padding: const EdgeInsets.all(6),
-            decoration: BoxDecoration(
-              color: leadingIconBackgroundColor ??
-                  Theme.of(context).colorScheme.surfaceContainerLow,
-              borderRadius: BorderRadius.circular(9),
+        Material(
+          child: ListTile(
+            tileColor:
+                tileColor ?? Theme.of(context).colorScheme.surfaceContainerLow,
+            titleTextStyle: Theme.of(
+              context,
+            ).textTheme.bodyLarge?.copyWith(color: titleColor),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
             ),
-            child: Icon(
-              leadingIcon.icon,
-              color: leadingIconColor ?? Theme.of(context).colorScheme.primary,
+            leading: Container(
+              padding: const EdgeInsets.all(6),
+              decoration: BoxDecoration(
+                color:
+                    leadingIconBackgroundColor ??
+                    Theme.of(context).colorScheme.surfaceContainerLow,
+                borderRadius: BorderRadius.circular(9),
+              ),
+              child: Icon(
+                leadingIcon.icon,
+                color:
+                    leadingIconColor ?? Theme.of(context).colorScheme.primary,
+              ),
             ),
-          ),
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text(title),
-              if (infoText != null)
-                Tooltip(
-                  message: infoText!,
-                  triggerMode: TooltipTriggerMode.tap,
-                  showDuration: const Duration(seconds: 5),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 4.0),
-                    child: Icon(
-                      Icons.help_outline_rounded,
-                      color: Theme.of(context).colorScheme.secondary,
-                      size: 18,
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(title),
+                if (infoText != null)
+                  Tooltip(
+                    message: infoText!,
+                    triggerMode: TooltipTriggerMode.tap,
+                    showDuration: const Duration(seconds: 5),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 4.0),
+                      child: Icon(
+                        Icons.help_outline_rounded,
+                        color: Theme.of(context).colorScheme.secondary,
+                        size: 18,
+                      ),
                     ),
                   ),
-                ),
-            ],
+              ],
+            ),
+            trailing: trailingWidget ?? trailingIcon,
+            onTap: onTap,
           ),
-          trailing: trailingWidget ?? trailingIcon,
-          onTap: onTap,
         ),
       ],
     );
