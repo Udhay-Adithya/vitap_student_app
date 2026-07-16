c870656bda4934bb89a52396e1309f42ea296d4d
Udhay-Adithya <udhayxd@gmail.com>
2026-07-16 23:47:37 +0530

    fix: update forest theme color to a greener tone

--- lib/core/theme/app_theme_enum.dart
+++ lib/core/theme/app_theme_enum.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 enum AppTheme {
   blue('Blue', Colors.blue),
   sakura('Sakura', Color.fromARGB(255, 252, 180, 241)),
-  forest('Forest', Color(0xFFB2C5B3)),
+  forest('Forest', Color.fromARGB(255, 251, 236, 106)),
   vapourWave('Vapour Wave', Color(0xFF1fc3ff)),
   nightfall('Nightfall', Color(0xFF0262A5)),
   cocoa('Cocoa', Color.fromARGB(255, 255, 202, 184));
