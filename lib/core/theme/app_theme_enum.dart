8f8b564f41a27cac60e7764da430b607c2d6ecfb
Udhay-Adithya <udhayxd@gmail.com>
2026-07-16 23:53:04 +0530

    feat: add new lemonade theme

--- lib/core/theme/app_theme_enum.dart
+++ lib/core/theme/app_theme_enum.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 enum AppTheme {
   blue('Blue', Colors.blue),
   sakura('Sakura', Color.fromARGB(255, 252, 180, 241)),
-  forest('Forest', Color.fromARGB(255, 251, 236, 106)),
+  forest('Forest', Color(0xFFB2C5B3)),
+  lime('Lemonade', Color.fromARGB(255, 251, 236, 106)),
   vapourWave('Vapour Wave', Color(0xFF1fc3ff)),
   nightfall('Nightfall', Color(0xFF0262A5)),
   cocoa('Cocoa', Color.fromARGB(255, 255, 202, 184));
